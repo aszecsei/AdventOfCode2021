499acb9aaa99d7f36a832688865b4975cfeb0f81
Alic Szecsei <aszecsei@gmail.com>
2021-11-30 21:31:40 -0800

    Add tests

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -12,15 +12,53 @@ pub fn input_generator_day1(input: &str) -> Vec<i64> {
 
 #[aoc(day1, part1)]
 pub fn solve_day1_part1(input: &[i64]) -> i64 {
-    input.windows(2).map(|win| if win[1] > win[0] { 1 } else { 0 }).sum()
+    input.windows(2)
+        .map(|win| if win[1] > win[0] { 1 } else { 0 })
+        .sum()
 }
 
 #[aoc(day1, part2)]
 pub fn solve_day1_part2(input: &[i64]) -> i64 {
-    input.windows(3).map(|win| win.iter().sum::<i64>()).collect_vec().windows(2).map(|win| if win[1] > win[0] { 1 } else { 0 }).sum()
+    input.windows(3)
+        .map(|win| win.iter().sum::<i64>())
+        .collect_vec()
+        .windows(2)
+        .map(|win| if win[1] > win[0] { 1 } else { 0 })
+        .sum()
 }
 
 #[test]
-fn test_day1() {
-    unimplemented!();
+fn test_day1_part1() {
+    let data = [
+        199,
+        200,
+        208,
+        210,
+        200,
+        207,
+        240,
+        269,
+        260,
+        263
+    ];
+    let result = solve_day1_part1(&data);
+    assert_eq!(result, 7);
+}
+
+#[test]
+fn test_day1_part2() {
+    let data = [
+        199,
+        200,
+        208,
+        210,
+        200,
+        207,
+        240,
+        269,
+        260,
+        263
+    ];
+    let result = solve_day1_part2(&data);
+    assert_eq!(result, 5);
 }
