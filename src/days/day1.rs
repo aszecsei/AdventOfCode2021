a89f8d739c361d0be5e433face23339a6be2b20a
Alic Szecsei <aszecsei@gmail.com>
2021-11-30 21:17:15 -0800

    Day 1

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -1,4 +1,5 @@
 use aoc_runner_derive::{aoc, aoc_generator};
+use itertools::Itertools;
 
 // ======================================================
 // DAY 1
@@ -6,17 +7,17 @@ use aoc_runner_derive::{aoc, aoc_generator};
 
 #[aoc_generator(day1)]
 pub fn input_generator_day1(input: &str) -> Vec<i64> {
-    unimplemented!();
+    input.lines().map(|x| x.trim().parse().unwrap()).collect()
 }
 
 #[aoc(day1, part1)]
 pub fn solve_day1_part1(input: &[i64]) -> i64 {
-    unimplemented!();
+    input.windows(2).map(|win| if win[1] > win[0] { 1 } else { 0 }).sum()
 }
 
 #[aoc(day1, part2)]
 pub fn solve_day1_part2(input: &[i64]) -> i64 {
-    unimplemented!();
+    input.windows(3).map(|win| win.iter().sum::<i64>()).collect_vec().windows(2).map(|win| if win[1] > win[0] { 1 } else { 0 }).sum()
 }
 
 #[test]
